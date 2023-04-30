0700756d308af84c35801397f9a96713b77f90a6
zermads <128172129+zermads@users.noreply.github.com>
2023-04-29 23:16:20 -0700

    Delete tools directory

--- tools/EncodeWebhook.py
+++
@@ -1,4 +0,0 @@
-import zlib, base64, codecs, lzma
-def encodatolol(wbh:str):
-    wbh=f'3+{base64.b85encode(codecs.encode(base64.b16encode(lzma.compress(wbh.encode())).decode(), "rot13").encode()).decode()}'
-    return wbh
